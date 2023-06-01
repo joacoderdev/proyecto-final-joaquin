182cdcce985ea2731aefcd982e46b82db07f4558
Joacoder <106751508+joacoderdev@users.noreply.github.com>
2023-06-01 14:46:54 -0300

    Add files via upload

---
+++ servidor.config.js
@@ -0,0 +1,10 @@
+export const PORT = 8080;
+export const mongooseConnectStringToAtlas = "mongodb+srv://joacoder:joacoderatlas@cluster0.9z13lnk.mongodb.net/ecommerce?retryWrites=true&w=majority";
+export const CORREO_EMAIL="joacoder.dev@gmail.com"
+export const PASSWORD_EMAIL="jockjsvaqbxmbkrs"
+
+
+
+
+
+
